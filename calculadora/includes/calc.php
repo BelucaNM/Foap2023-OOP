c072429441d4e11bf45080ef489e398e51fce13f
BelucaNM <beluca.navarrina@gmail.com>
2024-07-03 23:20:30 +0200

    commit

--- calculadora/includes/calc.php
+++ calculadora/includes/calc.php
@@ -7,12 +7,11 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST') && isset($_POST['submit']) ){
     $num1 = $_POST['numero1'];
     $num2 = $_POST['numero2'];
     $operacion = $_POST['operacion'];
-    echo 'TENEMOS CALCULADORA <br>';
-//  require "../controlador/calculadoraControlador.php";
-    require "autoload_controlador.php";
+    echo 'Creando instancia de CALCULADORA <br>';
+//  require "../controlador/calculadora.php";
+    require "../controlador/autoload_controlador.php";
     $calculadoraFoap= new calculadora($num1, $num2, $operacion);
-    echo 'TENEMOS CALCULADORA <br>';
-    echo $calculadoraFoap->calcular().'<br>';
+    echo 'Resultado '.$calculadoraFoap->calcular().'<br>';
 };
  
 ?>
