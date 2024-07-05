c614678086d3ad24750ba2ffca07a8d2f011fd1c
BelucaNM <beluca.navarrina@gmail.com>
2024-07-05 02:36:48 +0200

    commit

--- registro/modelo/usuario.php
+++ registro/modelo/usuario.php
@@ -4,11 +4,18 @@ class usuario extends connection {
         public function setUser($username, $password, $email) {
             
             $result = true;
-            $stmt = $this->connect()->prepare("INSERT INTO usuarios (username, password, email) VALUES (?,?,?)");
+//            $stmt = $this->connect()->prepare("INSERT INTO usuarios (username, password, email) VALUES (?,?,?)");
             
-            $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+//           $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
 
-            if(!$stmt->execute(array($username, $hashedPwd, $email))){
+//            if(!$stmt->execute(array($username, $hashedPwd, $email))){
+
+
+//                $result = false;
+//                }
+            $stmt = $this->connect()->query("INSERT INTO usuarios (username, password, email) VALUES ($username, $password, $email)");
+
+            if(!$stmt){
                 $result = false;
                 }
             $stmt = null;
