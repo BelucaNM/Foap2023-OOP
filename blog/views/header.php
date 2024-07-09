f09854e5f2ce965c8fbbab2f7d06a07413a15421
BelucaNM <beluca.navarrina@gmail.com>
2024-07-10 00:33:23 +0200

    commit

---
+++ blog/views/header.php
@@ -0,0 +1,7 @@
+<header>
+   <div id="header" class = "container mt-3 clearfix">
+        <span class="float-start"><img id="logo" src=".\imgCodigo\logo.jpg" class = "rounded-circle"></span>
+        <span class="h1">BLOG</span>
+        <span class="h1 float-end"><?=$title;?></span>
+    </div>
+</header>
